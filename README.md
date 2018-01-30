7e6ada7cdda626a145a4ed421b1ad50a54963972
betwowt <betwowt@163.com>
2018-01-30 19:25:54 +0800

    webpack css js separation

---
+++ README.md
@@ -0,0 +1,32 @@
+# webpack-vue-Demo
+
+> 项目来源于[慕课网](https://www.imooc.com/learn/935)课程,感谢[Jokcy](https://www.imooc.com/u/3083408/courses?sort=publish)老师的详细讲解
+>
+> 此项目基于webpack vue2.0 开发的Todo项目
+
+### how to start
+
+###### 1.project clone
+
+> git clone https://github.com/betwowt/webpack-vue-demo.git
+
+##### 2.install
+
+> npm install
+
+#####3.run dev 
+
+> npm run dev
+
+##### 4.run product
+
+> npm run build  
+>
+> html css js 会生成在dist目录中
+
+### vue chrome plugin
+
+> [vue插件地址](https://chrome.google.com/webstore/detail/nhdogjmejiglipccpnnnanhbledajbpd) 。需自备梯子
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -2,42 +2,25 @@ const path = require('path');
 //生成对应的html
 const HTMLPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const ExtractPlugin = require('extract-text-webpack-plugin');
+
 const isDev = process.env.NODE_ENV === 'development';
 const config = {
     target: "web",
-    entry: path.join(__dirname,'src/index.js'),
+    entry: path.join(__dirname, 'src/index.js'),
     output: {
-        filename: 'bundle.js',
-        path: path.join(__dirname,'dist')
+        filename: 'bundle.[hash:8].js',
+        path: path.join(__dirname, 'dist')
 
     },
     module: {
         rules: [{
             test: /\.vue/,
             loader: 'vue-loader'
-        },{
-            test: /\.css/,
-            use: [
-                'style-loader',
-                'css-loader'
-            ]
-        },{
+        }, {
             test: /\.jsx/,
             loader: 'babel-loader'
-        },{
-            test: /\.styl/,
-            use: [
-                'style-loader',
-                'css-loader',
-                {
-                    loader: "postcss-loader",
-                    options: {
-                        sourceMap: true,
-                    }
-                },
-                'stylus-loader'
-            ]
-        },{
+        }, {
             test: /\.(gif|jpg|jpeg|png|svg)$/,
             use: [
                 {
@@ -53,14 +36,28 @@ const config = {
     plugins: [
         new webpack.DefinePlugin({
             'process.env': {
-                NODE_ENV: isDev ? '"development"':'"production"'
+                NODE_ENV: isDev ? '"development"' : '"production"'
             }
         }),
         new HTMLPlugin()
     ]
 }
 
-if(isDev){
+if (isDev) {
+    config.module.rules.push({
+        test: /\.styl/,
+        use: [
+            'style-loader',
+            'css-loader',
+            {
+                loader: 'postcss-loader',
+                options: {
+                    sourceMap: true,
+                }
+            },
+            'stylus-loader'
+        ]
+    })
     config.devtool = '#cheap-module-eval-source-map'
     config.devServer = {
         port: 8000,
@@ -74,6 +71,38 @@ if(isDev){
     config.plugins.push(
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin()
+    )
+} else {
+    config.entry = {
+        app: path.join(__dirname, 'src/index.js'),
+        vendor: ['vue']
+    }
+    config.output.filename = '[name].[chunkhash:8].js'
+    config.module.rules.push(
+        {
+            test: /\.styl/,
+            use: ExtractPlugin.extract({
+                use: [
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                        }
+                    },
+                    'stylus-loader'
+                ]
+            })
+        },
+    )
+    config.plugins.push(
+        new ExtractPlugin('styles.[contentHash:8].css'),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor'
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'runtime'
+        })
     )
 }
 module.exports = config
